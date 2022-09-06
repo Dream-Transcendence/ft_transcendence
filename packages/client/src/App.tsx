74137a88ca4a8cf772ed3bc50f0a207b3bff87a4
doyun <flfns333@gmail.com>
2022-09-06 12:27:48 +0900

    ✨ feat(client): jsx 형식을 tsx로 변환

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavigationBar from './components/bar/NavigationBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
