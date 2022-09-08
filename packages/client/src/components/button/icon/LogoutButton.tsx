950c0c647cd6d8062445231ef059ddd919dac21b
한중명 <gkswndaud@gmail.com>
2022-09-08 14:45:12 +0900

    📈 perf(client): devide search bar from Navbar
    Navbar안에 있던 searchbox를 분리하였습니다

---
+++ packages/client/src/components/button/icon/LogoutButton.tsx
@@ -0,0 +1,12 @@
+import LogoutIcon from '@mui/icons-material/Logout';
+import { IconButton } from '@mui/material';
+
+function LogoutIconButton() {
+  return (
+    <IconButton aria-label="game" size="large">
+      <LogoutIcon fontSize="inherit" />
+    </IconButton>
+  );
+}
+
+export default LogoutIconButton;
