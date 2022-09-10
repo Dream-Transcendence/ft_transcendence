76be257b51a88ee540ded9680f1809d9928cf806
한중명 <gkswndaud@gmail.com>
2022-09-10 15:54:46 +0900

    ✨ feat(client/template): feat chat room default image (feat #52)
    간단하게 대기방 화면 이미지 템플릿을 만들었습니다. 반응형으로 작업하고자 여러 수정을 가하였는데, 아직 미숙하여  완벽하게 구현하지는 못했습니다. 향후 디자인 수정시, 다시 설정할
    예정입니다.
    
    feat #51

--- packages/client/src/template/ChatMainSection/ChatRoomDefaultTemplate.tsx
+++ packages/client/src/template/ChatMainSection/ChatRoomDefaultTemplate.tsx
@@ -11,12 +11,72 @@ const ChattingRoomDefaultLayout = styled('div')(({ theme }) => ({
   justifyContent: 'center',
   alignItems: 'center',
 }));
+const DefaultDesignLayout = styled('div')(({ theme }) => ({
+  width: '60%',
+  height: '50%',
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}));
 
+const TypoLayout = styled('div')(({ theme }) => ({
+  width: '30%',
+  height: '70%',
+  display: 'flex',
+}));
 function ChatRoomDefaultTemplate() {
   const [dense, setDense] = React.useState(false);
   const [secondary, setSecondary] = React.useState(false);
 
-  return <ChattingRoomDefaultLayout></ChattingRoomDefaultLayout>;
+  return (
+    <ChattingRoomDefaultLayout>
+      <DefaultDesignLayout>
+        <Typography
+          sx={{
+            width: '100%',
+            height: '100%',
+            fontSize: '1.0rem',
+            left: '20%',
+            '@media (min-width:1300px)': {
+              fontSize: '2.5rem',
+              left: '10%',
+            },
+            position: 'absolute',
+            bottom: '10%',
+          }}
+        >
+          친구와 대화를 시작해보세요
+        </Typography>
+        <SmsIcon
+          sx={{
+            width: '100%',
+            height: '100%',
+            color: '#4028BB',
+            position: 'absolute',
+          }}
+        />
+        <TypoLayout>
+          <Typography
+            sx={{
+              width: '100%',
+              height: '100%',
+              fontSize: '18vh',
+              top: '25%',
+              '@media (min-width:1000px)': {
+                fontSize: '20vh',
+                top: '6%',
+              },
+              fontSizeAdjust: 'from-font',
+              position: 'absolute',
+            }}
+          >
+            👫
+          </Typography>
+        </TypoLayout>
+      </DefaultDesignLayout>
+    </ChattingRoomDefaultLayout>
+  );
 }
 
 export default ChatRoomDefaultTemplate;
