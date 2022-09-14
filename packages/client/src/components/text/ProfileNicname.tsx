fed1abc78740e8873cb493797638670ad80737af
doyun <flfns333@gmail.com>
2022-09-14 16:03:05 +0900

    🐛 fix(client): fix UserInfo display

--- packages/client/src/components/text/ProfileNicname.tsx
+++ packages/client/src/components/text/ProfileNicname.tsx
@@ -17,59 +17,61 @@ import styled from '@emotion/styled';
 import { Edit } from '@mui/icons-material';
 import { TextField } from '@mui/material';
 
-const ProfileNicnameLayout = styled('text')(({ theme }) => ({
-  alignSelf: 'center',
-  width: '100%',
-  height: '60%',
-}));
+function EditableControls() {
+  const {
+    isEditing,
+    getSubmitButtonProps,
+    getCancelButtonProps,
+    getEditButtonProps,
+  } = useEditableControls();
+
+  return isEditing ? (
+    <ButtonGroup justifyContent="center" size="sm">
+      <IconButton
+        aria-label="ProfileNicname"
+        icon={<DoneIcon />}
+        {...getSubmitButtonProps()}
+      />
+      <IconButton
+        aria-label="ProfileNicname"
+        icon={<ClearIcon />}
+        {...getCancelButtonProps()}
+      />
+    </ButtonGroup>
+  ) : (
+    <Flex justifyContent="center">
+      <IconButton
+        aria-label="ProfileNicname"
+        size="sm"
+        icon={<SaveAltIcon />}
+        {...getEditButtonProps()}
+      />
+    </Flex>
+  );
+}
 
 function ProfileNicname() {
   /* Here's a custom control */
-  function EditableControls() {
-    const {
-      isEditing,
-      getSubmitButtonProps,
-      getCancelButtonProps,
-      getEditButtonProps,
-    } = useEditableControls();
 
-    return isEditing ? (
-      <ButtonGroup justifyContent="center" size="sm">
-        <IconButton
-          aria-label="Search database"
-          icon={<DoneIcon />}
-          {...getSubmitButtonProps()}
-        />
-        <IconButton
-          aria-label="Search database"
-          icon={<ClearIcon />}
-          {...getCancelButtonProps()}
-        />
-      </ButtonGroup>
-    ) : (
-      <Flex justifyContent="center">
-        <IconButton
-          aria-label="Search database"
-          size="sm"
-          icon={<SaveAltIcon />}
-          {...getEditButtonProps()}
-        />
-      </Flex>
-    );
-  }
+  const ProfileNicnameLayout = styled('text')(({ theme }) => ({
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  }));
 
   return (
-    //새로 고침시 값이 저장이 안됨 -> 직접 구현해야 할 듯
     <Editable
       textAlign="center"
-      defaultValue="Rasengan ⚡️"
+      defaultValue="sonkang ⚡️"
       fontSize="2xl"
       isPreviewFocusable={false}
     >
-      <EditablePreview />
-      {/* Here is the custom input */}
-      <Input as={EditableInput} />
-      <EditableControls />
+      <ProfileNicnameLayout>
+        <EditablePreview />
+        {/* Here is the custom input */}
+        <Input as={EditableInput} />
+        <EditableControls />
+      </ProfileNicnameLayout>
     </Editable>
   );
 }
