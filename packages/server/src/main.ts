4277ee19b2d7cb7fc826c4d3ef1048f205c8b926
Devdha <donghun.dev@gmail.com>
2022-09-22 22:53:02 +0900

    🐛 fix(server): comment out GlobalPipes(글로벌 파이프 주석처리)

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -6,13 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //     forbidNonWhitelisted: true,
+  //     transform: true,
+  //   }),
+  // );
   const config = new DocumentBuilder()
     .setTitle('API design example')
     .setDescription('Example')
