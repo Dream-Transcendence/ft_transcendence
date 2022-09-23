dd2602d2eb4c99e1fd8027185a6f8ca99ace1809
한중명 <gkswndaud@gmail.com>
2022-09-23 15:02:33 +0900

    🐛 fix(common): fix lint error
    no-used-vars에러를 해결하였습니다

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,7 @@ module.exports = {
   rules: {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
   },
 };
