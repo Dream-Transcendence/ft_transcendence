44021ab753384b8ad5e8428bc7f8d0c299ee3dc5
Devdha <donghun.dev@gmail.com>
2022-10-01 21:07:17 +0900

    ✨ feat(server): implement EntityNotFoundExceptionFilter (#92)
    When data not found in DB, throw EntityNotFoundError and the filter catches those exceptions.
    
    #92

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { EntityNotFoundExceptionFilter } from './filters/entity-not-found-exception.filter';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new EntityNotFoundExceptionFilter());
 
   // app.useGlobalPipes(
   //   new ValidationPipe({
