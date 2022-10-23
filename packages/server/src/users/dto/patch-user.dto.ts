1e061bf83f90a1a2aa499516bbfce59d1dfdef61
Devdha <donghun.dev@gmail.com>
2022-10-23 16:27:49 +0900

    ✨ feat(server/user): implement save user image on AWS S3 on patchImage (#219)
    #219

--- packages/server/src/users/dto/patch-user.dto.ts
+++ packages/server/src/users/dto/patch-user.dto.ts
@@ -1,14 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString, IsUrl, isURL } from 'class-validator';
 
-export class PatchUserDto {
+export class PatchImageDto {
+  @ApiProperty({ type: 'string', format: 'binary' })
+  file: any;
+}
+
+export class PatchNicknameDto {
   @ApiProperty({ example: 'dha' })
   @IsString()
   @IsOptional()
   nickname: string;
-
-  @ApiProperty({ example: 'https://cdn.42.fr/users/dha.jpg' })
-  // //@IsUrl()
-  @IsOptional()
-  image: string;
 }
