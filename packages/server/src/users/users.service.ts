6de948a35ec91189e0dbd5c60f927b1ccb7e82b0
Devdha <donghun.dev@gmail.com>
2022-10-24 17:51:53 +0900

    ✨ feat(server/user): block duplcate login (#242)
    #242

--- packages/server/src/users/users.service.ts
+++ packages/server/src/users/users.service.ts
@@ -32,7 +32,7 @@ import {
 import { v4 as uuidv4 } from 'uuid';
 import { Auth, Block, Friend, Request, User } from './users.entity';
 import { ConnectionDto, ConnectionsDto } from './dto/connect-user.dto';
-import { WsException } from '@nestjs/websockets';
+import { WebSocketGateway, WsException } from '@nestjs/websockets';
 import { MailerService } from '@nestjs-modules/mailer';
 import * as AWS from 'aws-sdk';
 
@@ -588,6 +588,11 @@ export class UserService {
 
   async handleLogOn(client: Socket, connectionDto: ConnectionDto) {
     const onlineUserList = Array.from(this.connectionList.values());
+    onlineUserList.map((connection) => {
+      if (connection.userId === connectionDto.userId) {
+        throw new WsException('이미 로그인 되어있습니다.');
+      }
+    });
 
     this.connectionList.set(client.id, {
       userId: connectionDto.userId,
