c984c58d93156a8cc716572617290b5bd92b9864
Donghwan Yun <flfns333@gmail.com>
2022-10-26 10:28:29 +0900

    ✨ feat(client): change game option button
    체크박스 옵션을 라디오 버튼으로 바꾸었습니다.

--- packages/client/src/molecules/GameSecton/GameOption.tsx
+++ packages/client/src/molecules/GameSecton/GameOption.tsx
@@ -1,61 +1,30 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import FormGroup from '@mui/material/FormGroup';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import FormHelperText from '@mui/material/FormHelperText';
-import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 function GameOption() {
-  const [state, setState] = React.useState({
-    sizeDown: true,
-    speedUp: false,
-  });
-  const { sizeDown, speedUp } = state;
-
+  const [state, setState] = React.useState('base');
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.name, '1' ,event.target.checked);
-    console.log('2', state);
-    setState({
-      ...state,
-      [event.target.name]: !(event.target.checked),
-    });
+    setState((event.target as HTMLInputElement).value);
   };
 
-  React.useEffect(() => {
-
-  }, [state])
-
   return (
-    <Box sx={{ display: 'flex' }}>
-      <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
-        <FormLabel component="legend">Game option</FormLabel>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Checkbox
-                // checked={sizeDown}
-                // onChange={handleChange}
-                name="sizeDown"
-              />
-            }
-            label="Size Down"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                // checked={speedUp}
-                // onChange={handleChange}
-                name="speedUp"
-              />
-            }
-            label="Speed Up"
-          />
-        </FormGroup>
-        <FormHelperText>옵션을 선택하세요.</FormHelperText>
-      </FormControl>
-    </Box>
+    <FormControl>
+      <FormLabel id="demo-controlled-radio-buttons-group">Game Option</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+        value={state}
+        onChange={handleChange}
+      >
+        <FormControlLabel value="base" control={<Radio />} label="base" />
+        <FormControlLabel value="sizeDown" control={<Radio />} label="sizeDown" />
+        <FormControlLabel value="speedUp" control={<Radio />} label="speedUp" />
+      </RadioGroup>
+    </FormControl>
   );
 }
 
