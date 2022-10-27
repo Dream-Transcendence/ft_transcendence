6d8c8c2073139af36ff1b7f3368a68cd900f4f49
Devdha <donghun.dev@gmail.com>
2022-10-27 17:22:03 +0900

    ✨ feat(server): add filter to error handling (#259)
    catch 400, 401, 404, 409 & typeorm errors not to server break
    
    #259

---
+++ packages/server/src/app.filter.ts
@@ -0,0 +1,115 @@
+import {
+  ArgumentsHost,
+  BadRequestException,
+  Catch,
+  ConflictException,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { NotFoundError } from 'rxjs';
+import {
+  QueryFailedError,
+  EntityNotFoundError,
+  CannotCreateEntityIdMapError,
+} from 'typeorm';
+
+export const GlobalResponseError: (
+  statusCode: number,
+  message: string,
+  code: string,
+  request: Request,
+) => IResponseError = (
+  statusCode: number,
+  message: string,
+  code: string,
+  request: Request,
+): IResponseError => {
+  return {
+    statusCode: statusCode,
+    message,
+    code,
+    timestamp: new Date().toISOString(),
+    path: request.url,
+    method: request.method,
+  };
+};
+
+export interface IResponseError {
+  statusCode: number;
+  message: string;
+  code: string;
+  timestamp: string;
+  path: string;
+  method: string;
+}
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    let message = (exception as any).message.message;
+    let code = 'HttpException';
+
+    Logger.error(
+      message,
+      (exception as any).stack,
+      `${request.method} ${request.url}`,
+    );
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    console.log('Error Message: ', message);
+
+    switch (exception.constructor) {
+      case HttpException:
+        status = (exception as HttpException).getStatus();
+        break;
+      case QueryFailedError: // this is a TypeOrm error
+        status = HttpStatus.UNPROCESSABLE_ENTITY;
+        message = (exception as QueryFailedError).message;
+        code = (exception as any).code;
+        break;
+      case EntityNotFoundError: // this is another TypeOrm error
+        status = HttpStatus.UNPROCESSABLE_ENTITY;
+        message = (exception as EntityNotFoundError).message;
+        code = (exception as any).code;
+        break;
+      case CannotCreateEntityIdMapError: // and another
+        status = HttpStatus.UNPROCESSABLE_ENTITY;
+        message = (exception as CannotCreateEntityIdMapError).message;
+        code = (exception as any).code;
+        break;
+      case UnauthorizedException:
+        status = HttpStatus.UNAUTHORIZED;
+        message = (exception as UnauthorizedException).message;
+        code = (exception as any).code;
+        break;
+      case ConflictException:
+        status = HttpStatus.CONFLICT;
+        message = (exception as ConflictException).message;
+        code = (exception as any).code;
+        break;
+      case NotFoundError:
+        status = HttpStatus.NOT_FOUND;
+        message = (exception as NotFoundError).message;
+        code = (exception as any).code;
+        break;
+      case BadRequestException:
+        status = HttpStatus.BAD_REQUEST;
+        message = (exception as BadRequestException).message;
+        code = (exception as any).code;
+        break;
+      default:
+        status = HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
+    response
+      .status(status)
+      .json(GlobalResponseError(status, message, code, request));
+  }
+}
