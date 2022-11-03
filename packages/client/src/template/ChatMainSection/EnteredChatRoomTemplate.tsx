20e07536bf156085266ef71c30e97a9fc6c6381f
jungmyeong96 <gkswndaud@gmail.com>
2022-11-03 11:32:41 +0900

    💎 style(client/chat): fix style for chat room info

--- packages/client/src/template/ChatMainSection/EnteredChatRoomTemplate.tsx
+++ packages/client/src/template/ChatMainSection/EnteredChatRoomTemplate.tsx
@@ -34,7 +34,7 @@ import { UserSecondAuth } from '../../types/Profile.type';
 
 const ChattingRoomLayout = styled('div')(({ theme }) => ({
   width: '100%',
-  height: '100%',
+  height: '98%',
 }));
 
 const ChattingBanLayout = styled('div')(({ theme }) => ({
