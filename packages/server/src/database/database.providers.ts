6fd5179507119f5e07deaea26d5fa8a3d8bf62a3
xhseb <zzzu5280@gmail.com>
2022-11-21 20:52:47 +0900

    ♻️ refactor(server): 노출되어있던 정보 env로 이동

--- packages/server/src/database/database.providers.ts
+++ packages/server/src/database/database.providers.ts
@@ -6,40 +6,18 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'host.docker.internal',
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         // NOTE: synchronize는 개발용으로만 사용하고, 배포시에는 false 로 설정해야 함
         synchronize: false,
-        logging: true,
+        logging: false,
         // NOTE console창에 query log를 볼 수 있는 option
       });
       return dataSource.initialize();
     },
   },
 ];
-
-// export const postgresDataSource = new DataSource({
-//   type: 'postgres',
-//   host: 'localhost',
-//   port: 5432,
-//   username: 'postgres',
-//   password: '1234',
-//   database: 'Transcendence',
-//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-//   synchronize: true,
-// });
-
-//  = {
-//   type: 'postgres',
-//   host: 'localhost',
-//   port: 5432,
-//   username: 'postgres',
-//   password: '1234',
-//   database: 'Transcendence',
-//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-//   synchronize: true,
-// };
